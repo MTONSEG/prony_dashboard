4edcaa6f54d9331a87b86fb918e0e16a55c6b0b3
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-05-30 13:03:45 +0300

    add edit rag screen

--- src/components/Main/TagScreen/EditTags/EditTags.jsx
+++ src/components/Main/TagScreen/EditTags/EditTags.jsx
@@ -6,6 +6,8 @@ import FormContainer from '../../../UI/Forms/FormContainer/FormContainer';
 import ColorBullets from '../ColorBullets/ColorBullets';
 import PrivacyTags from '../PrivacyTags/PrivacyTags';
 import InputFrom from '../../../UI/Forms/InputFrom/InputFrom';
+import ButtonsContainer from '../../../UI/Buttons/ButtonsContainer/ButtonsContainer';
+import Button from '../../../UI/Buttons/Button/Button';
 
 const EditTags = ({ add }) => {
 	const [state, setState] = useState(editTag);
@@ -13,6 +15,7 @@ const EditTags = ({ add }) => {
 	let value = add ? '' : state.name.value;
 
 	const [nameValue, setNameValue] = useState(value);
+	const [privacyValue, setPrivacyValue] = useState('public');
 
 	return (
 		<div className='edit-tag'>
@@ -31,7 +34,16 @@ const EditTags = ({ add }) => {
 					state={state}
 					setState={setState}
 				/>
-				<PrivacyTags />
+				<PrivacyTags
+					className='edit-tag__privacy'
+					state={state.privacy}
+					privacy={privacyValue}
+					setPrivacy={setPrivacyValue}
+				/>
+				<ButtonsContainer>
+					<Button color="grey">Cancel</Button>
+					<Button>Submit</Button>
+				</ButtonsContainer>
 			</FormContainer>
 		</div>
 	)
--- src/components/Main/TagScreen/PrivacyTags/PrivacyTags.jsx
+++ src/components/Main/TagScreen/PrivacyTags/PrivacyTags.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import RadioForm from '../../../UI/Forms/RadioForm/RadioForm';
 import './PrivacyTags.scss';
 
-const PrivacyTags = (props) => {
+const PrivacyTags = ({ state, className, privacy, setPrivacy }) => {
+
+	let radioList = state.list.map((el, i) => <RadioForm
+		key={i}
+		name={state.title}
+		title={el.title}
+		value={el.value}
+		checked={privacy}
+		setChecked={setPrivacy}
+		className={'privacy-tag__radio'}
+	/>)
 
 	return (
-		<div className='edit-tag'>
-
+		<div className={className
+			? `${className} privacy-tag`
+			: 'privacy-tag'
+		}>
+			<p className="privacy-tag__title">{state.title}</p>
+			<div className="privacy-tag__radio-list">
+				{radioList}
+			</div>
+			<p className="privacy-tag__text">{state.text}</p>
 		</div>
 	)
 }
